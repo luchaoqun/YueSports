b4f370f825f9007d5e44506a3e0aac0d5659258f
luchaoqun <luchaoqun@live.com>
2015-05-11 13:29:12 +0800

    First Commit

---
+++ app/src/main/java/com/yuesport/yuesports/MainActivity.java
@@ -0,0 +1,19 @@
+package com.yuesport.yuesports;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
